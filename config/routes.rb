c4dfb95291f1fafae68920e6aa611344a22ade98
AustinLinares <linaresaustin@hotmail.com>
2022-04-12 19:16:44 -0500

    deleting useless code and added to index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     root 'subscribers#new'
     resources :subscribers
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 end
