11ad75bb60e18e43be859b895bc5f7d94af8c71f
Claudia Berrios <cberriosledesma@gmail.com>
2022-04-13 18:49:37 -0500

    fixing final bugs

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -4,13 +4,19 @@ require "httparty"
 
 class Subscriber < ApplicationRecord
   def has_one_preference
-    errors.add(:preferences, I18n.t("messagges.errors.preferences_not_selected")) if preferences.empty?
+    if preferences.empty?
+      errors.add(:preferences,
+                 I18n.t("messages.errors.preferences_not_selected"))
+    end
   end
 
   def quality_score
     base_uri = "https://emailvalidation.abstractapi.com/v1/"
     response = HTTParty.get("#{base_uri}?api_key=#{ENV['ABSTRACT_API_KEY']}&email=#{self[:email]}", no_follow: true)
-    errors.add(:email, I18n.t("messagges.errors.score_not_valid")) if response["quality_score"].to_f < 0.7
+    if response["quality_score"].to_f < 0.7
+      errors.add(:email,
+                 I18n.t("messages.errors.score_not_valid"))
+    end
   end
   # Association
   has_and_belongs_to_many :preferences, dependent: :destroy
@@ -19,9 +25,9 @@ class Subscriber < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :email,
             format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,3}\z/,
-                      message: I18n.t("messagges.errors.format_not_valid") }, unless: proc { |a|
-                                                                                a.email.blank?
-                                                                              }
-  validate :has_one_preference, unless: proc { |a| a.email.blank?}
+                      message: I18n.t("messages.errors.format_not_valid") }, unless: proc { |a|
+                                                                                       a.email.blank?
+                                                                                     }
+  validate :has_one_preference, unless: proc { |a| a.email.blank? }
   validate :quality_score, unless: proc { |a| a.email.blank? }
 end
